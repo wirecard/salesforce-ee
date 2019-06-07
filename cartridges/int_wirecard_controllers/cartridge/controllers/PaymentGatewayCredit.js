8a22c7e87c133b85f9f866b6adb17792e8cd75c5
sru <s.ruedel@dotsource.de>
2019-06-07 14:48:33 +0200

    WIRE-56 fixed codacy issues

--- cartridges/int_wirecard_controllers/cartridge/controllers/PaymentGatewayCredit.js
+++ cartridges/int_wirecard_controllers/cartridge/controllers/PaymentGatewayCredit.js
@@ -229,9 +229,9 @@ exports.Notify = guard.ensure(['post', 'https'], function () {
     if (order && order.orderToken === orderToken) {
         // parse response
 
-        var requestBody       = parameterMap.getRequestBodyAsString();
+        var requestBody = parameterMap.getRequestBodyAsString();
         var transactionHelper = require('*/cartridge/scripts/paymentgateway/helper/TransactionHelper');
-        var notifyData        = transactionHelper.parseTransactionResponse(
+        var notifyData = transactionHelper.parseTransactionResponse(
             requestBody, null, transactionHelper.RESPONSE_TYPE_NOTIFY
         );
         var rawResponeJson = transactionHelper.getJsonSignedResponseWrapper(requestBody).getJsonResponse();
