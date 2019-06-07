cbedf87b1eb8a975291417686c4302964b19713d
Sebastian Ninse <sninse@dotsource.de>
2019-06-07 13:17:51 +0200

    fix frontend tests

---
+++ test/Object Repository/Payment methods/PayPal/Continue to Password.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Continue to Password</name>
+   <tag></tag>
+   <elementGuidId>f59d1f1d-ac5d-41d8-bc9f-bc98201e6e53</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//button[@id='btnNext'][contains(@class, 'actionContinue')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//button[@id='btnNext'][contains(@class, 'actionContinue')]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ test/Object Repository/Payment methods/PayPal/Password (split).rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Password (split)</name>
+   <tag></tag>
+   <elementGuidId>792eaab4-3dc0-4beb-a656-01504a3a6486</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@id='splitPassword']/div[@id='splitPasswordSection']//input[@id='password']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@id='splitPassword']/div[@id='splitPasswordSection']//input[@id='password']</value>
+   </webElementProperties>
+</WebElementEntity>
