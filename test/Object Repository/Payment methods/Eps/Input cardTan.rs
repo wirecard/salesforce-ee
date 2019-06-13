47a1bc208b278e63e7a5357a3ff592ee805d0072
Sebastian Ninse <sninse@dotsource.de>
2019-06-13 11:56:19 +0200

    WIRE-42 eps: frontend test

---
+++ test/Object Repository/Payment methods/Eps/Input cardTan.rs
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Input cardTan</name>
+   <tag></tag>
+   <elementGuidId>6903db11-c2d0-40c8-ac6d-4e9c8ddd3a42</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//div[@id='content']/form//div[@class='actions']//input[@name='cardTan']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>//div[@id='content']/form//div[@class='actions']//input[@name='cardTan']</value>
+   </webElementProperties>
+</WebElementEntity>
