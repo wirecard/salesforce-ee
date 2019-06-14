e8e7d4d9071f39eb28a5b5161cc5b7585dc36993
Sebastian Ninse <sninse@dotsource.de>
2019-06-14 12:03:16 +0200

    add (c) file headers

--- cartridges/int_wirecard_core/cartridge/scripts/paymentgateway/transaction/entity/Descriptor.js
+++ cartridges/int_wirecard_core/cartridge/scripts/paymentgateway/transaction/entity/Descriptor.js
@@ -1,3 +1,10 @@
+/**
+ * Shop System Plugins:
+ * - Terms of Use can be found under:
+ * https://github.com/wirecard/salesforce-ee/blob/master/_TERMS_OF_USE
+ * - License can be found under:
+ * https://github.com/wirecard/salesforce-ee/blob/master/LICENSE
+ */
 'use strict';
 
 var paymentHelper = require('int_wirecard_core/cartridge/scripts/paymentgateway/helper/PaymentHelper.js');
@@ -11,8 +18,7 @@ function Descriptor(transaction) {
     var order = transaction.order;
     var result = {};
     if ((transaction.paymentMethodID && transaction.paymentMethodID === paymentHelper.PAYMENT_METHOD_SOFORT)
-        || (transaction.paymentMethodID === paymentHelper.PAYMENT_METHOD_PAYPAL && transaction.getSitePreference('addDescriptorToRequest'))
-        || transaction.getSitePreference('sendAdditionalData')
+        || transaction.getSitePreference('addDescriptorToRequest')
     ) {
         var StringUtils = require('dw/util/StringUtils');
         result.descriptor = StringUtils.format(
