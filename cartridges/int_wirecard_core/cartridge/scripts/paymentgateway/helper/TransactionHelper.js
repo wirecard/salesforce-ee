2df8242f78e17c0d42c612bfe0118d3e7b630f16
Sebastian Ninse <sninse@dotsource.de>
2019-07-02 12:19:42 +0200

    WIRE-46 prevent replacement of parent transaction

--- cartridges/int_wirecard_core/cartridge/scripts/paymentgateway/helper/TransactionHelper.js
+++ cartridges/int_wirecard_core/cartridge/scripts/paymentgateway/helper/TransactionHelper.js
@@ -249,6 +249,7 @@ var TransactionHelper = {
                 }
             } else if (!transaction.parentTransactionId
                 && newTransaction.parentTransactionId == transaction.transactionId
+                && [paymentHelper.PAYMENT_METHOD_PAYOLUTION_INV].indexOf(transaction.paymentMethodId) === -1
             ) {
                 // replace initial transaction with notification response
                 allPaymentTransactions.push(JSON.stringify(newTransaction));
